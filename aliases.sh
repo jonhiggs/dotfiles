e2170fc50ade713bae5de1ee798c36c286eb8a4f
Jon Higgs <jon@toast.trikeapps.com>
2012-04-11 16:58:45 +1000

    add in some more aliases.

--- aliases.sh
+++ aliases.sh
@@ -1,2 +1,16 @@
+# VIM
 alias vi='vim'
 alias vim='vim --servername SCREEN --remote-tab'
+
+# GIT
+alias gg='gitg'
+alias gpom='git push origin master'
+alias gpu='git push'
+alias gpl='git pull'
+
+
+# MISC
+alias ack='ack-grep'
+
+# APT
+alias apt-get='sudo apt-get'
