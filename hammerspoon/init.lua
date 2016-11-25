8881b888939899c9e6b349c013beb95b4375c9c2
Jon Higgs <jhiggs@eml.cc>
2016-11-25 15:50:47 +1100

    Add Hammerspoon config

---
+++ hammerspoon/init.lua
@@ -0,0 +1,125 @@
+hs.window.animationDuration = 0
+
+local application = require "hs.application"
+
+hs.hotkey.bind({"cmd"}, "1", function()
+  application.launchOrFocus("iTerm")
+end)
+
+hs.hotkey.bind({"cmd"}, "2", function()
+  application.launchOrFocus("Google Chrome")
+end)
+
+hs.hotkey.bind({"cmd"}, "3", function()
+  application.launchOrFocus("Slack")
+end)
+
+-- hs.hotkey.bind({"alt"}, "B", function()
+--   local win = hs.window.focusedWindow()
+--   local app = win:application()
+-- 
+--   if not string.find(app:title(), "^iTerm2$") then
+--     print("sending alt")
+--     hs.eventtap.keyStroke({'alt'}, 'left')
+--   else
+--     print("sending real")
+--     hs.eventtap.keyStroke({'alt'}, 'B')
+--   end
+-- end)
+-- 
+-- hs.hotkey.bind({"alt"}, "F", function()
+--   local win = hs.window.focusedWindow()
+--   local app = win:application()
+-- 
+--   if not string.find(app:title(), "^iTerm2$") then
+--     print("sending alt")
+--     hs.eventtap.keyStroke({'alt'}, 'right')
+--   else
+--     print("sending real")
+--     hs.eventtap.keyStroke({'alt'}, 'F')
+--   end
+-- end)
+
+-- position fullscreen
+hs.hotkey.bind({"cmd", "shift"}, "F", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- position slim
+hs.hotkey.bind({"cmd", "shift"}, "D", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = 200
+  f.y = 0
+  f.w = 800
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+--position left
+hs.hotkey.bind({"cmd", "shift"}, "H", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- position right
+hs.hotkey.bind({"cmd", "shift"}, "L", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.w / 2
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- position top
+hs.hotkey.bind({"cmd", "shift"}, "K", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
+
+-- position bottom
+hs.hotkey.bind({"cmd", "shift"}, "J", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.h / 2
+  f.w = max.w
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
