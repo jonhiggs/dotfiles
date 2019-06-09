223058a76fd90e221d2186f1500a3131ad8f3a0d
Jon Higgs <jhiggs@eml.cc>
2019-06-09 11:32:55 +1000

    Update keyboard bindings

--- autokey/Readline/shell-backward-kill-word.py
+++ autokey/Readline/shell-backward-kill-word.py
@@ -2,6 +2,6 @@ winClass = window.get_active_class()
 exceptions = ["st-256color.st-256color"]
 
 if winClass not in exceptions:
-    keyboard.send_key("<left>")
+    keyboard.send_keys("<ctrl>+<backspace>")
 else:
-    keyboard.send_keys("<ctrl>+b")
+    keyboard.send_keys("<alt>+<backspace>")
--- autokey/paste.py
+++ autokey/paste.py
@@ -1,2 +1,7 @@
-keyboard.send_keys("<ctrl>+v")
+winClass = window.get_active_class()
+exceptions = ["st-256color.st-256color"]
 
+if winClass in exceptions:
+    keyboard.send_keys("<shift>+<insert>")
+else:
+    keyboard.send_keys("<ctrl>+v")
