930bb1ebb0b175fe283669805d38a3ae97d48d99
Jon Higgs <jhiggs@eml.cc>
2019-07-15 15:35:30 +1000

    Add Firefox start/end of line

--- autokey/Readline/beginning-of-line.py
+++ autokey/Readline/beginning-of-line.py
@@ -1,7 +1,10 @@
 winClass = window.get_active_class()
 exceptions = ["st-256color.st-256color"]
+ctrl_home = ["Navigator.Firefox"]
 
-if winClass not in exceptions:
-    keyboard.send_key("<home>")
-else:
+if winClass in ctrl_home:
+    keyboard.send_keys("<ctrl>+<home>")
+if winClass in exceptions:
     keyboard.send_keys("<ctrl>+a")
+else:
+    keyboard.send_key("<home>")
--- autokey/Readline/end-of-line.py
+++ autokey/Readline/end-of-line.py
@@ -1,7 +1,10 @@
 winClass = window.get_active_class()
 exceptions = ["st-256color.st-256color"]
+ctrl_end = ["Navigator.Firefox"]
 
-if winClass not in exceptions:
-    keyboard.send_key("<end>")
-else:
+if winClass in ctrl_end:
+    keyboard.send_keys("<ctrl>+<end>")
+elif winClass in exceptions:
     keyboard.send_keys("<ctrl>+e")
+else:
+    keyboard.send_key("<end>")
