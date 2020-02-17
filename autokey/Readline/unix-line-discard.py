2994da9708dde447e111575c61ca916ea26fead6
Jon Higgs <jhiggs@eml.cc>
2020-02-17 17:13:37 +1100

    Add some more AutoKey Readline bindings

---
+++ autokey/Readline/unix-line-discard.py
@@ -0,0 +1,9 @@
+winClass = window.get_active_class()
+exceptions = ["st-256color.st-256color"]
+
+if winClass not in exceptions:
+    keyboard.send_key("<home>")
+    keyboard.send_keys("<shift>+<end>")
+    keyboard.send_key("<delete>")
+else:
+    keyboard.send_keys("<ctrl>+u")
